4e6d0129a6d3c2f9d0b84240a356b81f549faa92
JeffWang <1620067299@qq.com>
2020-09-23 13:12:48 +0800

    remove dirty comment

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -14,12 +14,6 @@ public class Score : MonoBehaviour
     }
     void Update()
     {
-        // Debug.Log(player.position.z.ToString("0"));
-        // scoreText.text = "Time: " + (DateTime.Now - startTime); 
-        // scoreText.text = "Time: " + (DateTime.Now - startTime).ToString("MM/dd/yyyy hh:mm:ss.fff tt"); 
-        // TimeSpan time = DateTime.Now - startTime;
-        // scoreText.text = String.Format("{0}.{1}", time.Seconds, time.Milliseconds.ToString().PadLeft(3, '0'));
-        // scoreText.text = "Time: " + (DateTime.Now - startTime).Seconds.ToString() + " s"; 
         TimeSpan span = (TimeSpan)(DateTime.Now - startTime);
         scoreText.text = String.Format("Time: {0}.{1} s", span.Seconds, span.Milliseconds); 
     }
