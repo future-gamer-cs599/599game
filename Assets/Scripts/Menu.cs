b2c92cb2e7ac077d425c84fb7dcd2d9fbf1d7d96
yinil <yinil@usc.edu>
2020-09-24 17:31:48 -0700

    added level 5 using code from final test

--- Assets/Scripts/Menu.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using UnityEngine.SceneManagement;
-
-public class Menu : MonoBehaviour
-{
-    public void StartGame() {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }
-}
