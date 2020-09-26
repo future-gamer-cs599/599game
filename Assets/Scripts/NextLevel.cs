49d02a76fb4abcb8dd2be38ca8413f67518e0427
Fiona Tang <ftang1996@gmail.com>
2020-09-25 20:25:39 -0700

    Added level select screen
    modified nextLevel script to add level build index parameter.
    updated parameter for each level
    added level 4 + 5 to build index
    
    progress of player will be saved for level screen. To clear progres go to Edit > Clear PlayerPrefs

--- Assets/Scripts/NextLevel.cs
+++ Assets/Scripts/NextLevel.cs
@@ -3,7 +3,11 @@ using UnityEngine.SceneManagement;
 
 public class NextLevel : MonoBehaviour
 {
+    public int levelBuildIndex;
     public void LoadNextLevel() {
+        // save player progress
+        PlayerPrefs.SetInt("levelAt", levelBuildIndex + 1);
+
         // use build index
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1) ;
     }
