2983c68da58eba5669df4a07f3b97a3682ab3e84
Fiona Tang <ftang1996@gmail.com>
2020-09-26 14:43:07 -0700

    Exit button
    Added exit button to all levels to return to level home screen

--- Assets/Scripts/NextLevel.cs
+++ Assets/Scripts/NextLevel.cs
@@ -6,7 +6,9 @@ public class NextLevel : MonoBehaviour
     public int levelBuildIndex;
     public void LoadNextLevel() {
         // save player progress
-        PlayerPrefs.SetInt("levelAt", levelBuildIndex + 1);
+        if (PlayerPrefs.GetInt("levelAt") == levelBuildIndex) {
+            PlayerPrefs.SetInt("levelAt", levelBuildIndex + 1);
+        }
 
         // use build index
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1) ;
