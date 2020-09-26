75be766a5d79330946002333d37368872014fb5d
Fiona Tang <ftang1996@gmail.com>
2020-09-26 15:25:29 -0700

    WebGL Build

--- Assets/Scripts/NextLevel.cs
+++ Assets/Scripts/NextLevel.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class NextLevel : MonoBehaviour
@@ -6,10 +7,9 @@ public class NextLevel : MonoBehaviour
     public int levelBuildIndex;
     public void LoadNextLevel() {
         // save player progress
-        if (PlayerPrefs.GetInt("levelAt") == levelBuildIndex) {
-            PlayerPrefs.SetInt("levelAt", levelBuildIndex + 1);
-        }
-
+        int levelAt = Math.Max(PlayerPrefs.GetInt("levelAt"), levelBuildIndex + 1);
+        PlayerPrefs.SetInt("levelAt", levelAt);
+        
         // use build index
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1) ;
     }
