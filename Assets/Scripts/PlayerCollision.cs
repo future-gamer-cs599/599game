b94b9b1a66081f31b3728898371f4be53d58bc0b
Fiona Tang <ftang1996@gmail.com>
2020-10-03 14:56:51 -0700

    implemented levellost custom event

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -1,4 +1,8 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+using UnityEngine.SceneManagement;
 
 public class PlayerCollision : MonoBehaviour
 {
@@ -8,6 +12,26 @@ public class PlayerCollision : MonoBehaviour
         if (other.collider.tag == "Obstacle") {
             Debug.Log("It hurts! I'm colliding with: " + other.collider.name);
             movement.enabled = false;
+
+            // Log in anaytics
+            AnalyticsResult analyticsResult = Analytics.CustomEvent(
+                "LevelLost", new Dictionary<string, object> {
+                    // add level //
+                    { "Level", SceneManager.GetActiveScene().buildIndex - 1 },
+                    { "Obstacle", other.collider.name},
+                    { "X", GetComponent<Transform>().position.x },
+                    { "Y", GetComponent<Transform>().position.y },
+                    { "Z", GetComponent<Transform>().position.z }
+            });
+
+            // Debug analytics log
+            Debug.Log("CustomEvent LevelLost sent: " + analyticsResult);
+            Debug.Log("Level: " + (SceneManager.GetActiveScene().buildIndex - 1).ToString());
+            Debug.Log("Obstacle: " + other.collider.name);
+            Debug.Log("X: " + GetComponent<Transform>().position.x);
+            Debug.Log("Y: " + GetComponent<Transform>().position.y);
+            Debug.Log("Z: " + GetComponent<Transform>().position.z);
+
             FindObjectOfType<GameManager>().EndGame();
         }
         else if (other.collider.tag == "Block") {
