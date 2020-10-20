e48c6d75452dbc0dbc9bcf25bd5c06e5fe2476df
James Xu <54513773+fbjxu@users.noreply.github.com>
2020-10-20 08:22:25 -0700

    updated joystick script to include analytics, end, keyboard

--- Assets/Scripts/JoyStickMove.cs
+++ Assets/Scripts/JoyStickMove.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Analytics;
+using UnityEngine.SceneManagement;
 
 public class JoyStickMove : MonoBehaviour
 {
@@ -25,16 +27,31 @@ public class JoyStickMove : MonoBehaviour
     {
         var rigidbody = GetComponent<Rigidbody>();
         rigidbody.velocity = new Vector3(joystick.Horizontal * moveSpeed + Input.GetAxis("Horizontal") * moveSpeed, rigidbody.velocity.y, joystick.Vertical * moveSpeed + Input.GetAxis("Vertical") * moveSpeed);
-        if(!jump && joybutton.Pressed)
+        // if the player falls off, restart the game
+        if (rigidbody.position.y < -0.5f)
+        {
+            FindObjectOfType<GameManager>().EndGame("Fell");
+        }
+        if ((!jump && joybutton.Pressed) || (!jump && Input.GetKeyDown(KeyCode.Space)))
         {
             if (JumpCount > 0)
             {
                 jump = true;
                 rigidbody.velocity += Vector3.up * jumpSpeed;
                 JumpCount -= 1;
+                /***********************/
+                /****** ANALYTICS ******/
+                /***********************/
+                // Analytics Log: jump occurrences of a level
+                AnalyticsResult jumpAnalytics = Analytics.CustomEvent(
+                    "Jump", new Dictionary<string, object> {
+                { "Level", SceneManager.GetActiveScene().buildIndex - 1 },
+                });
+                // Debug: Analytics
+                Debug.Log("CustomEvent Jump sent: " + jumpAnalytics);
             }
         }
-        if(jump && !joybutton.Pressed)
+        if((jump && !joybutton.Pressed) || (jump && !Input.GetKeyDown(KeyCode.J)))
         {
             jump = false;
         }
