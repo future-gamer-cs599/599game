138e4ab25535e7772f2bf50d0b4819facc961bfe
Fiona Tang <ftang1996@gmail.com>
2020-10-22 17:00:52 -0700

    collision soundeffect

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -7,13 +7,19 @@ using UnityEngine.SceneManagement;
 public class PlayerCollision : MonoBehaviour
 {
     public PlayerMovement movement;
+    public AudioSource sound;
 
     void OnCollisionEnter(Collision other){
         if (other.collider.tag == "Obstacle") {
             Debug.Log("It hurts! I'm colliding with: " + other.collider.name);
+
+            // Sound effect
+
+            sound.Play();
+
+
             movement.enabled = false;
-            FindObjectOfType<GameManager>().EndGame(other.collider.name);
-        }
+            FindObjectOfType<GameManager>().EndGame(other.collider.name);        }
         else if (other.collider.tag == "Block") {
         	Debug.Log("Wrong way! I'm walking towards: " + other.collider.name);
             // movement.enabled = false;
