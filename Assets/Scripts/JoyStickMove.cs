405c0f3dbbb8f6c484c19b8a0d31353f3d3f3959
JeffWang <1620067299@qq.com>
2020-11-09 23:50:28 +0800

    add rotation

--- Assets/Scripts/JoyStickMove.cs
+++ Assets/Scripts/JoyStickMove.cs
@@ -13,6 +13,10 @@ public class JoyStickMove : MonoBehaviour
     protected bool jump;
     int JumpCount = 0;
     public int MaxJumps = 1; //Maximum amount of jumps (i.e. 2 for double jumps)
+
+    public float TurnSpeed = 40f;
+    Quaternion playerRotation = Quaternion.identity;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,19 +29,38 @@ public class JoyStickMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        var rigidbody = GetComponent<Rigidbody>();
-        rigidbody.velocity = new Vector3(joystick.Horizontal * moveSpeed + Input.GetAxis("Horizontal") * moveSpeed, rigidbody.velocity.y, joystick.Vertical * moveSpeed + Input.GetAxis("Vertical") * moveSpeed);
+        var rigidbody = GetComponent<Rigidbody>(); // get rigidbody of the player
+
+        // change valocity according to input from keyboard or joystick
+        GetComponent<Rigidbody>().velocity = new Vector3(
+            joystick.Horizontal * moveSpeed + Input.GetAxis("Horizontal") * moveSpeed, 
+            GetComponent<Rigidbody>().velocity.y, 
+            joystick.Vertical * moveSpeed + Input.GetAxis("Vertical") * moveSpeed
+        );
+
+        // move rotation
+        float horizontal = joystick.Horizontal + Input.GetAxis("Horizontal");
+        float vertical = joystick.Vertical + Input.GetAxis("Vertical");
+        Vector3 movementVec3 = new Vector3();
+        movementVec3.Set(horizontal, 0f, vertical);
+        movementVec3.Normalize();
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, movementVec3, TurnSpeed * Time.deltaTime, 0f);
+        playerRotation = Quaternion.LookRotation(desiredForward);
+        rigidbody.MoveRotation(playerRotation);
+
         // if the player falls off, restart the game
-        if (SceneManager.GetActiveScene().buildIndex != 6 && rigidbody.position.y < -0.5f)
+        if (SceneManager.GetActiveScene().buildIndex != 6 && GetComponent<Rigidbody>().position.y < -0.5f)
         {
             FindObjectOfType<GameManager>().EndGame("Fell");
         }
+
+        // jump 
         if ((!jump && joybutton.Pressed) || (!jump && Input.GetKeyDown(KeyCode.Space)))
         {
             if (JumpCount > 0)
             {
                 jump = true;
-                rigidbody.velocity += Vector3.up * jumpSpeed;
+                GetComponent<Rigidbody>().velocity += Vector3.up * jumpSpeed;
                 JumpCount -= 1;
                 /***********************/
                 /****** ANALYTICS ******/
