53f6e95bfe1de58f38c90335604cb82c985cae1e
James Xu <54513773+fbjxu@users.noreply.github.com>
2020-12-02 22:26:14 -0800

    added highscore board

--- Assets/Scripts/EndTrigger.cs
+++ Assets/Scripts/EndTrigger.cs
@@ -4,12 +4,35 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Analytics;
+using UnityEngine.UI;
 public class EndTrigger : MonoBehaviour
 {
     public GameManager gameManager;
     public AudioSource winningSound;
+    public JoyStickMove movement;
     public float timeStart;
     bool updateTimer = true;
+
+    public Text score;
+    public Text bestScore;
+    public GameObject scoreBoard;
+    public GameObject timeDisplayed;
+
+    private void Start()
+    {
+        scoreBoard.SetActive(false);
+        timeDisplayed.SetActive(true);
+        var levelName = (SceneManager.GetActiveScene().buildIndex - 1).ToString();
+        if (PlayerPrefs.GetFloat(levelName + "HighScore", 0f) == 0)
+        {
+            bestScore.text = "Not Available";
+        }
+        else
+        {
+            bestScore.text = PlayerPrefs.GetFloat(levelName + "HighScore", 0f).ToString("F2") + "s";
+        }
+
+    }
 
     void Update()
     {
@@ -26,8 +49,18 @@ public class EndTrigger : MonoBehaviour
         {
             winningSound.Play();
             updateTimer = false;
+            movement.enabled = false;
             Debug.Log("end hit");
-            gameManager.completeLevel();
+            var levelName = (SceneManager.GetActiveScene().buildIndex - 1).ToString();
+            if (timeStart < PlayerPrefs.GetFloat(levelName + "HighScore", 0f))
+            {
+                PlayerPrefs.SetFloat(levelName + "HighScore", timeStart);
+            }
+            scoreBoard.SetActive(true);//show scoreboard
+            timeDisplayed.SetActive(false);
+            score.text = timeStart.ToString("F2") + "s";
+
+            //gameManager.completeLevel();
         }
 
         AnalyticsResult timeAnalytics = Analytics.CustomEvent(
@@ -38,7 +71,7 @@ public class EndTrigger : MonoBehaviour
 
         Debug.Log("CustomEvent LevelTime sent: " + timeAnalytics);
 
-  
-        
+
+
     }
 }
---
+++ Assets/Scripts/Scoreboard/gotoNextButton.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Analytics;
+
+
+
+public class gotoNextButton : MonoBehaviour
+{
+    public GameManager gameManager;
+    public void Home()
+    {
+        Debug.Log("complete level");
+        gameManager.completeLevel();
+    }
+}
